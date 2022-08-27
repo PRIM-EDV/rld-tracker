93bd5a149363a4d286e5a739744224d7fcec679f
Lucas Moesch <lucasmoesch@live.de>
2022-08-27 10:39:27 +0200

    Fixed old xpcc licensing in libraries

--- src/lib/bluetooth/src/bluetooth_header.hpp
+++ src/lib/bluetooth/src/bluetooth_header.hpp
@@ -2,8 +2,6 @@
 /* Copyright (c) 2018, Lucas Moesch
  * All Rights Reserved.
  *
- * The file is part of the xpcc library and is released under the 3-clause BSD
- * license. See the file `LICENSE` for the full license governing this code.
  */
 // ----------------------------------------------------------------------------
 
--- src/lib/gps/src/gps.hpp
+++ src/lib/gps/src/gps.hpp
@@ -2,8 +2,6 @@
 /* Copyright (c) 2017, Lucas Mösch
  * All Rights Reserved.
  *
- * The file is part of the xpcc library and is released under the 3-clause BSD
- * license. See the file `LICENSE` for the full license governing this code.
  */
 // ----------------------------------------------------------------------------
 
