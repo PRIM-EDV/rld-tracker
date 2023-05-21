0fdf2742e1b68e8c7018fc977b5c2df98b7b11ac
Lucas Moesch <lucasmoesch@live.de>
2023-05-21 12:21:35 +0200

    Added c_cpp properties and basic GPS NMEA implementation

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,11 +6,24 @@
 #include "board/board.hpp"
 #include "src/threads/lora.hpp"
 
+#include "driver/gps/protocol/nmea0183.hpp"
+
+using namespace Board;
+namespace Board::lora{
+    LoraThread<Spi, Nss, D0> thread;
+}
+
 int main()
 {
     Board::initialize();
 
-    while(true) {
+    NMEA0183<Board::gps::Uart> nmea;
+    // nmea.message = nullptr;
 
+    lora::thread.initialize();
+
+    while(true) {
+        lora::thread.run();
+        
     }
 }
